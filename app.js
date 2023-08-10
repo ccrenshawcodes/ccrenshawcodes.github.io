0753fb2b968c32a5a107a01c17d84a4193bda66f
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2023-08-10 16:23:53 -0600

    Add images with links to other pages

---
+++ app.js
@@ -0,0 +1,27 @@
+const library = document.querySelector('.library');
+const ttt = document.querySelector('.ttt');
+const dash = document.querySelector('.dash');
+
+library.addEventListener('click', () => {
+    window.open('https://ccrenshawcodes.github.io/odin-library/');
+})
+
+ttt.addEventListener('click', () => {
+    window.open('https://ccrenshawcodes.github.io/odin-tictactoe/');
+})
+
+dash.addEventListener('click', () => {
+    window.open('https://ccrenshawcodes.github.io/odin-admin-dash/');
+})
+
+const github = document.querySelector('.github');
+const linkedin = document.querySelector('.linkedin');
+const email = document.querySelector('.email');
+
+github.addEventListener('click', () => {
+    window.open('https://github.com/ccrenshawcodes');
+});
+
+linkedin.addEventListener('click', () => {
+    window.open('https://www.linkedin.com/in/catherine-ann/');
+})
