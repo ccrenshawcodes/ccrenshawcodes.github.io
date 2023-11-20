668af11b4e202de638449f5cdfa047da2c4b8560
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2023-11-20 15:16:42 -0700

    Change appearance and layout

--- app.js
+++ app.js
@@ -1,27 +1,4 @@
-const library = document.querySelector('.library');
-const ttt = document.querySelector('.ttt');
-const dash = document.querySelector('.dash');
-
-library.addEventListener('click', () => {
-    window.open('https://ccrenshawcodes.github.io/odin-library/');
-})
-
-ttt.addEventListener('click', () => {
-    window.open('https://ccrenshawcodes.github.io/odin-tictactoe/');
-})
-
-dash.addEventListener('click', () => {
-    window.open('https://ccrenshawcodes.github.io/odin-admin-dash/');
-})
-
-const github = document.querySelector('.github');
-const linkedin = document.querySelector('.linkedin');
-const email = document.querySelector('.email');
-
-github.addEventListener('click', () => {
-    window.open('https://github.com/ccrenshawcodes');
-});
-
-linkedin.addEventListener('click', () => {
-    window.open('https://www.linkedin.com/in/catherine-ann/');
+window.addEventListener('load', () => {
+    const title = document.querySelector('.my-name');
+    title.style.cssText = 'opacity: 100%;';
 })
